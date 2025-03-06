97f3171014d5a7c219f13715cac9c0c5d6402ee2
MehmetAliAkcali <mehmetali.akcali@std.yeditepe.edu.tr>
2025-03-06 20:10:18 +0300

    Bmw.cs

--- bmw.cs
+++ bmw.cs
@@ -1,4 +1,4 @@
-class BMW:Araba,IYetenekler {
+class BMW:Araba {
    public void YetenekleriYazdir(string isim)
         {
             Console.WriteLine(isim +": Çok hızlı gider, denizde yüzer, havada uçar.");
